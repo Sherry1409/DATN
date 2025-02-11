444a2a79ddcf614bdf568e66d353fbc808d5d096
Sherry1409 <rokuro688@gmail.com>
2025-02-11 21:59:26 +0700

    base

---
+++ src/types/Product.ts
@@ -0,0 +1,16 @@
+export type Product = {
+  id: string
+  title: string
+  description: string
+  price: number
+  brand: string
+  thumbnail: string
+}
+
+export type ProductFormValue = {
+  title: string
+  description: string
+  brand: string
+  price: string
+  thumbnail: string
+}
